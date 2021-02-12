07aee61f1c9a4894b8121cbec0d5de1154e39f12
wtsegase411 <78131224+wtsegase411@users.noreply.github.com>
2021-02-11 21:01:18 -0500

    Delete InvalidSequenceException.java

--- InvalidSequenceException.java
+++
@@ -1,9 +0,0 @@
-
-public class InvalidSequenceException extends RuntimeException {
-	public InvalidSequenceException() {
-		super();
-	}
-	public InvalidSequenceException(String message) {
-		super(message);
-	}
-}
