bb2500f5665d04d562f4bd724039cf0d9f7e5618
wtsegase411 <78131224+wtsegase411@users.noreply.github.com>
2021-02-11 21:02:17 -0500

    Delete NoSpecialCharacterException.java

--- NoSpecialCharacterException.java
+++
@@ -1,9 +0,0 @@
-
-public class NoSpecialCharacterException extends Exception {
-	public NoSpecialCharacterException() {
-		super();
-	}
-	public NoSpecialCharacterException(String message) {
-		super(message);
-	}
-}
